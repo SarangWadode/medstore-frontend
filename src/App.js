22c6246ee2c587ed4aa0734f54f7c8a21cb6af47
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2021-03-26 22:30:02 +0530

    Updated README

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import styled from 'styled-components';
 import AndroidIcon from '@material-ui/icons/Android';
 
 function App() {
   return (
-    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <Sarang>Hello from <AndroidIcon/></Sarang>
       </header>
     </div>
@@ -27,7 +14,6 @@ function App() {
 
 export default App;
 const Sarang = styled.main`
-
---fire: #f00;
-color: var(--fire);
-`
+  --fire: #f00;
+  color: var(--fire);
+`;
